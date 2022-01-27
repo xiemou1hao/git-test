ccb78c7a5cb876324d27b633145a18064e4ff2cb
xiemou1hao <97892240+xiemou1hao@users.noreply.github.com>
2022-01-27 18:41:22 +0800

    pull commit

--- src/main/java/com/xiemou/git/GitTest.java
+++ src/main/java/com/xiemou/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("Hello, Git4!");
         System.out.println("master test!");
         System.out.println("push test!");
+        System.out.println("github edit!");
     }
 }
