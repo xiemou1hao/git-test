2c8524ae76d1a7cd1c909d4409ded5413f933ee2
xiemou <xiemou@xiemou.com>
2022-01-27 16:50:36 +0800

    first commit

---
+++ src/main/java/com/xiemou/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xiemou.git;
+
+/**
+ * @author xiemou
+ * @create 2022-01-27 16:48
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+    }
+}
